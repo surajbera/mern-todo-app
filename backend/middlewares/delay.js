2dae52dd14a38079b6008d0165bf7d4e6140ad81
suraj.be <suraj.be@media.net>
2023-01-18 13:54:24 +0530

    removed dev comments

--- backend/middlewares/delay.js
+++ backend/middlewares/delay.js
@@ -1,8 +1,6 @@
 /* Tutor says that is a factory function, but I think this is currying */
 module.exports = (milliseconds = 1) => (req, res, next) => {
-  console.log('Middleware function will be executed');
   setTimeout(() => {
-    console.log(`Delay middleware function ran after ${milliseconds} milliseconds`);
     next()
   }, milliseconds)
 }
